df9554bfca8ab5a5605b94d730ceba2a60ba712e
Anton Aliokhna <antonaliokhna@gmail.com>
2022-09-11 09:19:04 +0300

    Add model tests

---
+++ NetworkProjectTests/Models/WebsiteTests.swift
@@ -0,0 +1,17 @@
+//
+//  WebsiteTests.swift
+//  NetworkProjectTests
+//
+//  Created by Anton Aliokhna on 9/11/22.
+//
+
+import XCTest
+@testable import NetworkProject
+
+class WebsiteTests: XCTestCase {
+
+    func testInitWebsiteWitchoutUsingParameters() {
+        let website = Website(courses: nil, websiteName: nil, websiteDescription: nil)
+        XCTAssertNotNil(website)
+    }
+}
